8b26bee8f6bcd69fc823b80b1be4e3ccf9d9d57f
WalidKhalqallah <wkhalqallah2@myges.fr>
2024-10-21 04:40:22 +0200

    fix

--- src/app/components/logo/index.tsx
+++ src/app/components/logo/index.tsx
@@ -1,18 +1,26 @@
 "use client"
+import { usePathname, useRouter } from "next/navigation";
 import { FC } from "react";
 
 interface LogoProps {
 
 }
 
+const router = useRouter();
+const pathname = usePathname();
+
 const Logo: FC<LogoProps> = () => {
     return (
         <div className="px-8 py-6 cursor-pointer " onClick={
             () => {
-                window.scrollTo({
-                    top: 0,
-                    behavior: "smooth"
-                })
+                if (pathname !== "/") {
+                    window.scrollTo({
+                        top: 0,
+                        behavior: "smooth"
+                    })
+                } else {
+                    router.push("/");
+                }
             }
         }>
             <h1 className="text-amber-100 text-4xl font-logo">Folklore</h1>
